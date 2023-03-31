af428a1efa4eb7ba89b9be0ebd930215a41c9f19
RhaissaZeferino <rhanalialves@gmail.com>
2023-03-31 16:11:23 -0300

    Update: Finishing the pages that we need to our application based on the first step

--- app/src/main/java/com/example/projetocondominio/PaginaInicial.java
+++ app/src/main/java/com/example/projetocondominio/PaginaInicial.java
@@ -1,21 +1,17 @@
 package com.example.projetocondominio;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class PaginaInicial extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_pagina_inicial);
 
         super.getSupportActionBar().hide();
 
     }
-
-
 }
